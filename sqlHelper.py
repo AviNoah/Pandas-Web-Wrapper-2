8433a0746361eac911a6c41972564a9b2d3abb4a
Avi Noah <AviNoah2019@gmail.com>
2024-01-30 03:04:22 +0200

    Implemented DB class

--- sqlHelper.py
+++ sqlHelper.py
@@ -3,8 +3,20 @@ import os
 
 
 class DB:
-    def __init__(self, DB_Path: os.PathLike):
-        self.__conn = sqlite3.connect(DB_Path)
+    def __init__(self, DB_Path: os.PathLike, auto_commit: bool = False):
+        if auto_commit:
+            isolation_level = None  # Auto-commit mode
+        else:
+            isolation_level = ""  # Default isolation level
+        self.__conn: sqlite3.Connection = sqlite3.connect(
+            DB_Path, isolation_level=isolation_level
+        )
 
     def conn(self):
         return self.__conn
+
+    def commit(self):
+        self.__conn.commit()
+
+    def close(self):
+        self.__conn.close()
