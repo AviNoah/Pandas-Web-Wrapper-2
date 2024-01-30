54acea6ad97fe083fb1f4132c31afdb10eb4fb4f
Avi Noah <AviNoah2019@gmail.com>
2024-01-30 04:26:04 +0200

    filter out invalid ext

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import tempfile
 import pandas as pd
 
 from sqlHelper import *
+from helperMethods import isAValidExt
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
@@ -50,6 +51,7 @@ def get_template(template):
 def file_upload():
     # Save files into database.
     files: list = list(request.files.values())
+    files: list = list(filter(isAValidExt, files))
 
     file_statuses: list = list()
     for file in files:
--- helperMethods.py
+++ helperMethods.py
@@ -3,7 +3,7 @@
 import os
 
 
-def is_valid_ext(filename: str, ALLOWED_EXTENSIONS: set) -> bool:
+def isAValidExt(filename: str, ALLOWED_EXTENSIONS: set) -> bool:
     _, ext = os.path.splitext(os.path.basename(filename))  # discard name
     if not ext:
         return False  # No extension isn't valid.
