947819b5fc8b6e071baa07dd9808cb5f17cfdb5a
Avi Noah <AviNoah2019@gmail.com>
2024-02-01 13:28:33 +0200

    Added file_handler

---
+++ static/javascripts/file_manager/file_handler.js
@@ -0,0 +1,19 @@
+function handleSelect(event, dataId) {
+    console.log("handleSelect executed with dataId:", dataId);
+}
+
+function handleEdit(event, dataId) {
+    console.log("handleEdit executed with dataId:", dataId);
+}
+
+function handleQueryList(event, dataId) {
+    console.log("handleQueryList executed with dataId:", dataId);
+}
+
+function handleDownload(event, dataId) {
+    console.log("handleDownload executed with dataId:", dataId);
+}
+
+function handleDelete(event, dataId) {
+    console.log("handleDelete executed with dataId:", dataId);
+}
