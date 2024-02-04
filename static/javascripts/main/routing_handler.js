4abddc566f928d5263eb60f470b97f6a539091fd
Avi Noah <AviNoah2019@gmail.com>
2024-02-04 15:28:40 +0200

    Added ids to the iframes, added routing_handler

---
+++ static/javascripts/main/routing_handler.js
@@ -0,0 +1,4 @@
+// Handle routing data from the iframes
+const spreadsheetFrame = document.getElementById('spreadsheetFrame');
+const fileManagerFrame = document.getElementById('fileManagerFrame');
+
