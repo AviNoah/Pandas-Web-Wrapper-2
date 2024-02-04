2fdc9ae445bfe430b37bdf36208d62f1d8ac6337
Avi Noah <AviNoah2019@gmail.com>
2024-02-04 15:32:08 +0200

    Forwarded message between iframes

--- static/javascripts/main/routing_handler.js
+++ static/javascripts/main/routing_handler.js
@@ -2,3 +2,10 @@
 const spreadsheetFrame = document.getElementById('spreadsheetFrame');
 const fileManagerFrame = document.getElementById('fileManagerFrame');
 
+window.addEventListener('message', (event) => {
+    if (event.origin !== fileManagerFrame)
+        return;  // Don't accept any messages not from fileManagerFrame
+
+    // Forward message data to spreadsheetFrame
+    spreadsheetFrame.contentWindow.postMessage(event.data, event.origin);
+})
