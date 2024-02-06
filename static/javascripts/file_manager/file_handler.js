23eab4b76b32600bd3f98ad94558c2c65cc55e4a
Avi Noah <AviNoah2019@gmail.com>
2024-02-06 23:56:33 +0200

    Fixed saving file names in non english chars

--- static/javascripts/file_manager/file_handler.js
+++ static/javascripts/file_manager/file_handler.js
@@ -134,24 +134,41 @@ function handleDownload(dataId) {
             if (!response.ok)
                 throw new Error("Server did not find file");
 
-            // Extract filename from Content-Disposition header
-            const contentDisposition = response.headers.get('Content-Disposition');
-            let filename = contentDisposition.split('filename=')[1].trim();
-
-            filename = filename.replace(/^_+|_+$"/g, '').replace(/^"+|"+$/g, '');  // Remove trailing white
-
-            response.blob().then(blob => {
-                // Handle the Blob data, for example, create a URL for downloading
-                const url = window.URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = filename;
-                document.body.appendChild(a);
-                a.click();
-                window.URL.revokeObjectURL(url);
-            });
+            return response.blob();
         })
+        .then(blob => {
+            fetch("/files/get/name", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: data
+            })
+                .then(response => {
+                    if (!response.ok)
+                        throw new Error("Server did not find file");
 
+                    return response.json();
+                })
+                .then(json => {
+                    if (!json.hasOwnProperty("name"))
+                        throw new Error("Response json doesn't have name key");
+
+                    return json.name;
+                })
+                .then(filename => {
+
+                    // Handle the Blob data, for example, create a URL for downloading
+                    const url = window.URL.createObjectURL(blob);
+                    const a = document.createElement('a');
+                    a.href = url;
+                    a.download = filename;
+                    document.body.appendChild(a);
+                    a.click();
+                    window.URL.revokeObjectURL(url);
+
+                })
+        })
         .catch(error => console.error(error));
 
 }
