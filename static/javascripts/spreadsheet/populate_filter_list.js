25bd6b107739ad0163c8e014f4c73eb2d79f6162
Avi Noah <AviNoah2019@gmail.com>
2024-02-07 02:38:26 +0200

    Added close filter pop up

--- static/javascripts/spreadsheet/populate_filter_list.js
+++ static/javascripts/spreadsheet/populate_filter_list.js
@@ -3,6 +3,28 @@ import { addFilter } from "/scripts/spreadsheet/filter_handler.js";
 export function populateFilterList(event, column) {
     const popUp = createPopup(column); // Make popup
     positionPopup(event.target, popUp); // position it under filter img
+    document.addEventListener('click', (event) => closeFilterPopup(event));  // Listen to closing
+}
+
+function closeFilterPopup(event) {
+    // Function to handle closing the filter popup
+    const filterPopup = document.querySelector('.filters-list-container');
+
+    // Check if the clicked element or its parent is outside the filter popup
+    if (!filterPopup)
+        return;  // Not initialized yet
+
+    if (event.target === filterPopup || filterPopup.contains(event.target)) {
+        return;  // An element inside filterPopup was selected
+    }
+
+    // An element outside of filterPopup was selected
+    closePopupHelper(filterPopup);
+}
+
+function closePopupHelper(filterPopup) {
+    filterPopup.style.display = 'none';
+    document.removeEventListener('click', closeFilterPopup);
 }
 
 function positionPopup(target, popup) {
