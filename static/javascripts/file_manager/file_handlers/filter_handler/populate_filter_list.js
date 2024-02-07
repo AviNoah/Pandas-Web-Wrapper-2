7d91728e3879127227993ab49f8125a9274f6b19
Avi Noah <AviNoah2019@gmail.com>
2024-02-07 02:26:19 +0200

    Continuing to implement populate filter list

--- static/javascripts/file_manager/file_handlers/filter_handler/populate_filter_list.js
+++ static/javascripts/file_manager/file_handlers/filter_handler/populate_filter_list.js
@@ -3,17 +3,12 @@ function handleFilterList(fileView, dataId) {
 
     const sheet = new Promise(getSheet());
     const filters = sheet.then(sheet => new Promise(getFilters(dataId, sheet)));
-    const filterListDiv = filters.then(filtersList => new Promise(populateFilterList(filtersList)));
+    const container = filters.then(filtersList => new Promise(populateFilterList(filtersList)));
 
-    filterListDiv.then(div => {
-        fileView.appendChild(div);
-        const container = document.createElement('div');
-        container.classList.add('filters-list-container');
-
+    container.then(container => {
+        // Adjust position and make visible
+        fileView.appendChild(container);
         container.setAttribute('display', 'block');
-
-        // Adjust position
-
     })
 }
 
@@ -25,7 +20,7 @@ function getSheet() {
 function getFilters(fileId, sheet) {
     const data = JSON.stringify({ fileId: fileId, sheet: sheet });
 
-    fetch('/filters/get/for_sheet', {
+    return fetch('/filters/get/for_sheet', {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -39,13 +34,29 @@ function getFilters(fileId, sheet) {
             return response.json();
         })
         .then(json => {
-            Array.from(json).forEach(filter => {
-                // Filter is a dict
-
-            })
+            return Array.from(json);
         })
+        .catch(error => console.error(error));
 }
 
 function populateFilterList(filters) {
 
+    return fetch('/templates/filter/filter.html')
+        .then(response => {
+            if (!response.ok)
+                throw new Error("Could not retrieve filter template");
+
+            return response.text;
+        })
+        .then(content => {
+            const container = document.createElement('div');
+            container.classList.add('filters-list-container');
+
+            filters.forEach(filter => {
+                // Filter contains the following keys: input, method, enabled, column
+                
+            })
+
+            return container
+        })
 }
