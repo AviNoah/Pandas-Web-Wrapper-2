2dcff0aefb30938c460dd992ed97c4ccdb80c3f8
Avi Noah <AviNoah2019@gmail.com>
2024-02-11 03:58:07 +0200

    Optional methods do not require try catch block now since context manager will handle them and return None; All methods now use the new cursor() method and will commit before closing the cursor. removed auto_commit parameter; init_db doesnt run DB() method, simply creates db file.

--- sqlHelper.py
+++ sqlHelper.py
@@ -53,7 +53,7 @@ class ConnectionPool:
             self.pool.put(connect(self.db_path))
         self.pool_size += value
 
-    def get_connection(self):
+    def get_connection(self) -> Connection:
         with self.lock:
             if self.pool.empty():
                 self._increase_pool_size(3)  # Allocate more connections due to load
@@ -65,16 +65,9 @@ class ConnectionPool:
 
 
 class DB:
-
-    def __init__(
-        self, db_path: os.PathLike, auto_commit: bool = True, pool_size: int = 5
-    ):
-        self.auto_commit = auto_commit
+    def __init__(self, db_path: os.PathLike, pool_size: int = 5):
         self.connection_pool = ConnectionPool(db_path, pool_size)
         self.init_tables()
-
-    def conn(self) -> Connection:
-        return self.__conn
 
     @contextmanager
     def cursor(self) -> Generator[Cursor]:
@@ -87,8 +80,7 @@ class DB:
             raise e
         finally:
             cursor.close()
-            if self.auto_commit:
-                conn.commit()
+            conn.commit()
             self.connection_pool.release_connection(conn)
 
     def init_tables(self):
@@ -124,159 +116,125 @@ class DB:
                             UNIQUE({FileFilterColumns.FILE_ID.value}, {FileFilterColumns.FILTER_ID.value}))"""
             )
 
-    def rollback(self):
-        self.conn().rollback()
-
-    def close(self):
-        self.__conn.close()
-
     def add_file(self, filename: str, file: FileStorage) -> Tuple[bool, str, int]:
         # Add the file_blob to database
         filename = os.path.basename(filename)
         name, ext = os.path.splitext(filename)
-        c: Cursor = self.cursor()
-        try:
-            c.execute(
-                f"""INSERT INTO {Tables.File.value} 
-                ({FileColumns.NAME.value}, 
-                {FileColumns.EXT.value}, 
-                {FileColumns.BLOB.value})
-                VALUES (?, ?, ?)""",
-                (name, ext, file.read()),
-            )
+        with self.cursor() as c:
+            try:
+                c.execute(
+                    f"""INSERT INTO {Tables.File.value} 
+                    ({FileColumns.NAME.value}, 
+                    {FileColumns.EXT.value}, 
+                    {FileColumns.BLOB.value})
+                    VALUES (?, ?, ?)""",
+                    (name, ext, file.read()),
+                )
 
-            file_id = c.lastrowid
-            self.commit()
-            return True, f"Added {name} successfully", file_id
-        except Error as e:
-            self.rollback()
-            return False, f"Failed to add {name}: {e}", None
+                file_id = c.lastrowid
+                return True, f"Added {name} successfully", file_id
+            except Error as e:
+                return False, f"Failed to add {name}: {e}", None
 
     def add_filter(
         self, method: str, input: str, enabled: bool
     ) -> Tuple[bool, str, int]:
-        c: Cursor = self.cursor()
-        try:
-            c.execute(
-                f"""INSERT INTO {Tables.Filter.value} 
-                ({FilterColumns.METHOD.value}, 
-                {FilterColumns.INPUT.value}, 
-                {FilterColumns.ENABLED.value})
-                VALUES (?, ?, ?)""",
-                (method, input, enabled),
-            )
+        with self.cursor() as c:
+            try:
+                c.execute(
+                    f"""INSERT INTO {Tables.Filter.value} 
+                    ({FilterColumns.METHOD.value}, 
+                    {FilterColumns.INPUT.value}, 
+                    {FilterColumns.ENABLED.value})
+                    VALUES (?, ?, ?)""",
+                    (method, input, enabled),
+                )
 
-            filter_id = c.lastrowid
-            self.commit()
-            return True, f"Added filter successfully", filter_id
-        except Error as e:
-            self.rollback()
-            return False, f"Failed to add filter: {e}", None
+                filter_id = c.lastrowid
+                return True, f"Added filter successfully", filter_id
+            except Error as e:
+                return False, f"Failed to add filter: {e}", None
 
     def file_filter_relationship(
         self, file_id: int, filter_id: int, sheet: int, column: int
     ) -> Tuple[bool, str]:
-        c: Cursor = self.cursor()
-        try:
-            c.execute(
-                f"""INSERT OR REPLACE INTO {Tables.FileFilter.value} 
-                ({FileFilterColumns.FILE_ID.value},
-                {FileFilterColumns.FILTER_ID.value},
-                {FileFilterColumns.SHEET.value},
-                {FileFilterColumns.COLUMN.value})
-                VALUES (?, ?, ?, ?)""",
-                (file_id, filter_id, sheet, column),
-            )
-            self.commit()
-            return True, f"Relationship created successfully"
-        except Error as e:
-            self.rollback()
-            return False, f"Failed to create relationship: {e}"
+        with self.cursor() as c:
+            try:
+                c.execute(
+                    f"""INSERT OR REPLACE INTO {Tables.FileFilter.value} 
+                    ({FileFilterColumns.FILE_ID.value},
+                    {FileFilterColumns.FILTER_ID.value},
+                    {FileFilterColumns.SHEET.value},
+                    {FileFilterColumns.COLUMN.value})
+                    VALUES (?, ?, ?, ?)""",
+                    (file_id, filter_id, sheet, column),
+                )
+
+                return True, f"Relationship created successfully"
+            except Error as e:
+                return False, f"Failed to create relationship: {e}"
 
     def get_file(self, file_id) -> Optional[FileStorage]:
         # Return file blob as FileStorage
-        c: Cursor = self.cursor()
-
-        try:
+        with self.cursor() as c:
             c.execute(
                 f"""SELECT 
-                {FileColumns.BLOB.value}, 
-                {FileColumns.NAME.value},
-                {FileColumns.EXT.value} 
-                FROM {Tables.File.value}
-                WHERE {FileColumns.ID.value}=?""",
+                    {FileColumns.BLOB.value}, 
+                    {FileColumns.NAME.value},
+                    {FileColumns.EXT.value} 
+                    FROM {Tables.File.value}
+                    WHERE {FileColumns.ID.value}=?""",
                 (file_id,),
             )
             blob, name, ext = c.fetchone()
             return FileStorage(
                 blob, filename=name + ext, content_type="application/octet-stream"
             )
-        except Error as e:
-            return None  # Failed to fetch file
 
     def get_file_name(self, file_id) -> Optional[tuple[str, str]]:
         # Return file name and ext
-        c: Cursor = self.cursor()
-
-        try:
+        with self.cursor() as c:
             c.execute(
                 f"""SELECT 
-                {FileColumns.NAME.value},
-                {FileColumns.EXT.value} 
-                FROM {Tables.File.value}
-                WHERE {FileColumns.ID.value}=?""",
+                    {FileColumns.NAME.value},
+                    {FileColumns.EXT.value} 
+                    FROM {Tables.File.value}
+                    WHERE {FileColumns.ID.value}=?""",
                 (file_id,),
             )
             name, ext = c.fetchone()
             return name, ext
-        except Error as e:
-            return None  # Failed to fetch file
 
     def get_all_files(self) -> Optional[List[dict]]:
-        c: Cursor = self.cursor()
-
-        try:
+        with self.cursor() as c:
             c.execute(
                 f"""SELECT 
-                    {FileColumns.ID.value}, 
-                    {FileColumns.NAME.value},
-                    {FileColumns.EXT.value} 
-                    FROM {Tables.File.value}"""
+                        {FileColumns.ID.value}, 
+                        {FileColumns.NAME.value},
+                        {FileColumns.EXT.value} 
+                        FROM {Tables.File.value}"""
             )
 
             files: List[dict] = [
-                {
-                    "id": id,
-                    "name": name,
-                    "ext": ext,
-                }
-                for id, name, ext in c.fetchall()
+                {"id": id, "name": name, "ext": ext} for id, name, ext in c.fetchall()
             ]
 
             return files
-        except Error as e:
-            return None  # Failed to fetch files
 
     def get_all_file_ids(self) -> Optional[List[int]]:
-        c: Cursor = self.cursor()
-
-        try:
+        with self.cursor() as c:
             c.execute(
                 f"""SELECT 
-                    {FileColumns.ID.value}
-                    FROM {Tables.File.value}"""
+                        {FileColumns.ID.value}
+                        FROM {Tables.File.value}"""
             )
             rows = c.fetchall()
             files: List[int] = [id for id, *_ in rows]
             return files
-        except Error as e:
-            return None  # Failed to fetch files
 
     def get_filter(self, filter_id) -> Optional[dict]:
         # Return a json representing filter data
-        c: Cursor = self.cursor()
-
-        try:
+        with self.cursor() as c:
             c.execute(
                 f"""SELECT {FilterColumns.INPUT.value}, 
                 {FilterColumns.METHOD.value}, 
@@ -296,23 +254,19 @@ class DB:
                 "column": column,
             }
             return data
-        except Exception as e:
-            return None  # Filter not found
 
     def get_sheets_filters(self, file_id, sheet) -> Optional[List[dict]]:
         # Return a json representing a list of filter data's
-        c: Cursor = self.cursor()
-
-        try:
+        with self.cursor() as c:
             c.execute(
                 f"""SELECT {FilterColumns.INPUT.value}, 
-                {FilterColumns.METHOD.value}, 
-                {FilterColumns.ENABLED.value}, 
-                {Tables.FileFilter.value}.{FileFilterColumns.COLUMN.value}
-                FROM {Tables.Filter.value}
-                LEFT JOIN {Tables.FileFilter.value}
-                ON {Tables.Filter.value}.{FilterColumns.ID.value} = {Tables.FileFilter.value}.{FileFilterColumns.FILTER_ID.value}
-                WHERE {Tables.FileFilter.value}.{FileFilterColumns.FILE_ID.value}=? AND {Tables.FileFilter.value}.{FileFilterColumns.SHEET.value}=?""",
+                    {FilterColumns.METHOD.value}, 
+                    {FilterColumns.ENABLED.value}, 
+                    {Tables.FileFilter.value}.{FileFilterColumns.COLUMN.value}
+                    FROM {Tables.Filter.value}
+                    LEFT JOIN {Tables.FileFilter.value}
+                    ON {Tables.Filter.value}.{FilterColumns.ID.value} = {Tables.FileFilter.value}.{FileFilterColumns.FILTER_ID.value}
+                    WHERE {Tables.FileFilter.value}.{FileFilterColumns.FILE_ID.value}=? AND {Tables.FileFilter.value}.{FileFilterColumns.SHEET.value}=?""",
                 (
                     file_id,
                     sheet,
@@ -330,14 +284,10 @@ class DB:
                     }
                 )
             return filters_data
-        except Exception as e:
-            return None  # Filters not found
 
     def get_filters_at(self, file_id, sheet, column) -> Optional[List[dict]]:
         # Return a json representing a list of filter data's
-        c: Cursor = self.cursor()
-
-        try:
+        with self.cursor() as c:
             c.execute(
                 f"""SELECT {FilterColumns.ID.value}, 
                 {FilterColumns.INPUT.value}, 
@@ -367,49 +317,41 @@ class DB:
                     }
                 )
             return filters_data
-        except Exception as e:
-            return None  # Filters not found
 
     def update_filter(self, filter_id, method: str, input: str, enabled: bool) -> bool:
-        # Update filter data matching id
-        c: Cursor = self.cursor()
+        # Update filter data matching id, return whether succeeded or not
+        with self.cursor() as c:
+            try:
+                c.execute(
+                    f"""UPDATE {Tables.Filter.value}
+                    SET {FilterColumns.INPUT.value} = ?,
+                        {FilterColumns.METHOD.value} = ?,
+                        {FilterColumns.ENABLED.value} = ?
+                    WHERE {FilterColumns.ID.value} = ?""",
+                    (input, method, enabled, filter_id),
+                )
 
-        try:
-            c.execute(
-                f"""UPDATE {Tables.Filter.value}
-                SET {FilterColumns.INPUT.value} = ?,
-                    {FilterColumns.METHOD.value} = ?,
-                    {FilterColumns.ENABLED.value} = ?
-                WHERE {FilterColumns.ID.value} = ?""",
-                (input, method, enabled, filter_id),
-            )
-
-            self.commit()
-            if c.rowcount > 0:
-                return True
-            return False
-        except Exception as e:
-            self.rollback()
-            return False  # Filter not found
+                if c.rowcount > 0:
+                    return True
+                return False
+            except Exception as e:
+                return False  # Filter not found
 
     def delete_filter(self, filter_id) -> bool:
-        # Delete filter record matching id
-        c: Cursor = self.cursor()
+        # Delete filter record matching id, return whether succeeded or not
+        with self.cursor() as c:
+            try:
+                c.execute(
+                    f"""DELETE FROM {Tables.Filter.value}
+                    WHERE {FilterColumns.ID.value} = ?""",
+                    (filter_id,),
+                )
 
-        try:
-            c.execute(
-                f"""DELETE FROM {Tables.Filter.value}
-                WHERE {FilterColumns.ID.value} = ?""",
-                (filter_id,),
-            )
-
-            self.commit()
-            if c.rowcount > 0:
-                return True
-            return False
-        except Exception as e:
-            self.rollback()
-            return False  # Filter not found
+                if c.rowcount > 0:
+                    return True
+                return False
+            except Exception as e:
+                return False  # Filter not found
 
     def update_file(
         self, file_id, filename: str, file: FileStorage
@@ -417,64 +359,55 @@ class DB:
         # Update the file_blob in database at the given id
         filename = os.path.basename(filename)
         name, ext = os.path.splitext(filename)
-        c: Cursor = self.cursor()
-        try:
-            c.execute(
-                f"""UPDATE {Tables.File.value}
-                SET {FileColumns.NAME.value} = ?,
-                    {FileColumns.EXT.value} = ?,
-                    {FileColumns.BLOB.value} = ?
-                WHERE {FileColumns.ID.value} = ?""",
-                (name, ext, file.read(), file_id),
-            )
+        with self.cursor() as c:
+            try:
+                c.execute(
+                    f"""UPDATE {Tables.File.value}
+                    SET {FileColumns.NAME.value} = ?,
+                        {FileColumns.EXT.value} = ?,
+                        {FileColumns.BLOB.value} = ?
+                    WHERE {FileColumns.ID.value} = ?""",
+                    (name, ext, file.read(), file_id),
+                )
 
-            self.commit()
-
-            return c.rowcount > 0, f"Updated {name} successfully", file_id
-        except Error as e:
-            self.rollback()
-            return False, f"Failed to update {name}: {e}", file_id
+                return c.rowcount > 0, f"Updated {name} successfully", file_id
+            except Error as e:
+                return False, f"Failed to update {name}: {e}", file_id
 
     def update_file_name(self, file_id, name) -> Tuple[bool, str, int]:
         # Update the file name and ext in database at the given id
-        c: Cursor = self.cursor()
-        try:
-            c.execute(
-                f"""UPDATE {Tables.File.value}
-                SET {FileColumns.NAME.value} = ?
-                WHERE {FileColumns.ID.value} = ?""",
-                (name, file_id),
-            )
+        with self.cursor() as c:
+            try:
+                c.execute(
+                    f"""UPDATE {Tables.File.value}
+                    SET {FileColumns.NAME.value} = ?
+                    WHERE {FileColumns.ID.value} = ?""",
+                    (name, file_id),
+                )
 
-            self.commit()
-
-            return c.rowcount > 0, f"Updated {name} successfully", file_id
-        except Error as e:
-            self.rollback()
-            return False, f"Failed to update {name}: {e}", file_id
+                return c.rowcount > 0, f"Updated {name} successfully", file_id
+            except Error as e:
+                return False, f"Failed to update {name}: {e}", file_id
 
     def delete_file(self, file_id) -> bool:
         # Delete file record matching id
-        c: Cursor = self.cursor()
+        with self.cursor() as c:
+            try:
+                c.execute(
+                    f"""DELETE FROM {Tables.File.value}
+                    WHERE {FileColumns.ID.value} = ?""",
+                    (file_id,),
+                )
 
-        try:
-            c.execute(
-                f"""DELETE FROM {Tables.File.value}
-                WHERE {FileColumns.ID.value} = ?""",
-                (file_id,),
-            )
-
-            self.commit()
-            if c.rowcount > 0:
-                return True
-            return False
-        except Exception as e:
-            self.rollback()
-            return False  # File not found
+                if c.rowcount > 0:
+                    return True
+                return False
+            except Exception as e:
+                return False  # File not found
 
 
 def init_db(parent: os.PathLike, db_name: str) -> os.PathLike:
-    # Initialize DB and return path
+    # Initialize DB file and return path
     if parent and not os.path.exists(parent):
         os.mkdir(parent)
 
@@ -484,9 +417,5 @@ def init_db(parent: os.PathLike, db_name: str) -> os.PathLike:
     if not os.path.exists(db_path):
         with open(db_path, "a"):
             pass
-
-    tmp = DB(db_path)
-    tmp.commit()
-    tmp.close()
 
     return db_path
