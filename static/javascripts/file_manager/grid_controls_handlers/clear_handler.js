a2b5bd83b50343ddfa241065d946a182a163bcec
Avi Noah <AviNoah2019@gmail.com>
2024-02-23 00:58:44 +0200

    Implemented handleClear; added todo

--- static/javascripts/file_manager/grid_controls_handlers/clear_handler.js
+++ static/javascripts/file_manager/grid_controls_handlers/clear_handler.js
@@ -1,3 +1,11 @@
 function handleClear() {
-    console.log(1)
+    // TODO: After implementing sessions, clear these files from session files
+    const dropzone = document.getElementById("drop-zone");
+    const choice = confirm("Clear grid? this will remove all files from session!");
+
+    if (!choice)
+        return;
+
+    while (dropzone.children.length !== 0)
+        dropzone.removeChild(dropzone.firstChild);
 }
